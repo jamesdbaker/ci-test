74e68907bcab9ad9cdd8808ce75971972159d9a5
James Baker <james.d.baker@gmail.com>
2019-02-01 15:18:31 +0000

    Improve greeting selection, add unit test

--- src/test/java/io/github/jamesdbaker/HelloWorldTest.java
+++ src/test/java/io/github/jamesdbaker/HelloWorldTest.java
@@ -1,6 +1,7 @@
 package io.github.jamesdbaker;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +11,10 @@ public class HelloWorldTest {
     assertEquals("world", HelloWorld.getName(new String[]{}));
     assertEquals("James", HelloWorld.getName(new String[]{"James"}));
     assertEquals("James", HelloWorld.getName(new String[]{"James", "Jane"}));
+  }
+
+  @Test
+  public void testGetGreeting(){
+    assertNotNull(HelloWorld.getGreeting());
   }
 }
